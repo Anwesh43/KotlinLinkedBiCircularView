f7156fd248d7635ded9be7706ecdb0eda3a35401
Anwesh43 <anweshthecool0@gmail.com>
2018-06-08 09:13:00 +0530

    adding Animator class

--- linkedbicircularview/src/main/java/com/anwesh/uiprojects/linkedbicircularview/LinkedBiCircularView.kt
+++ linkedbicircularview/src/main/java/com/anwesh/uiprojects/linkedbicircularview/LinkedBiCircularView.kt
@@ -49,6 +49,33 @@ class LinkedBiCircularView(ctx : Context) : View(ctx) {
                 startcb()
             }
         }
+    }
 
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
     }
 }

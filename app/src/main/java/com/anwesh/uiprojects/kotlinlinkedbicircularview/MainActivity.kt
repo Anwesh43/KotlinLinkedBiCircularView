395d5643145e3e05cb5ceb35296768f8cfa0fcce
Anwesh43 <anweshthecool0@gmail.com>
2018-06-08 10:03:23 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedbicircularview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedbicircularview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.kotlinlinkedbicircularview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.linkedbicircularview.LinkedBiCircularView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LinkedBiCircularView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }

366ab119acdf61d3b6c0e41e794a981705aea513
Anwesh43 <anweshthecool0@gmail.com>
2018-06-08 09:45:26 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedbicircularview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedbicircularview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedbicircularview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedbicircularview.LinkedBiCircularView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedBiCircularView.create(this)
     }
 }
